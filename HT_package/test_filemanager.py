58af9b2d258498d3f042fe6b4cf13a1332285440
VitalZH <gmavik@gmail.com>
2022-11-11 21:57:50 +0300

    Добавлен файл est_python.py

--- HT_package/test_filemanager.py
+++ HT_package/test_filemanager.py
@@ -6,7 +6,7 @@ assert hello_who('Leo') == 'Hello, Leo', 'Hello who error'
 assert salary(2, 1) == 4, salary(2, 1)
 assert salary(2, 2) == 8
 
-from def_for_test_class import function_for_test
+from HT_package.def_for_test_class import function_for_test
 
 list_in = [1, 2, 3, 'abc']
 assert function_for_test(list_in) == ' error'
--- HT_package/test_python_second.py
+++ HT_package/test_python_second.py
@@ -1,4 +1,4 @@
-from HT_9_victory import get_random_person
+from HT_package.HT_9_victory import get_random_person
 
 def test_get_random_person():
-    assert get_random_person() == {'Александр Сергеевич Пушнин': '26.06.1799'}
+    assert get_random_person() == ('Александр Сергеевич Пушнин', '26.06.1799')
