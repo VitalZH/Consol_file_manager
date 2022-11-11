ee260b3716cb3921dc629e90962924d50e2b3b9c
VitalZH <gmavik@gmail.com>
2022-11-11 17:48:55 +0300

    Добавлен файл est_python.py

--- HT_package/HT_10_def_schet.py
+++ HT_package/HT_10_def_schet.py
@@ -1,20 +1,3 @@
-# Ниже функция для выполнения ДЗ "Тестирование функций" не относится к телу программы "Счет"
-def function_for_test(list_in):
-    list_temp = []
-    for i in range(len(list_in)):
-        if type(list_in[i]) == int:
-            list_temp.append(list_in[i])
-    return list_temp
-
-def hello_who(name):
-    return f'Hello, {name}'
-
-
-def salary(hours, salary_by_hour):
-    k = 2
-    return hours * salary_by_hour * k
-
-
 # начало программы Счет
 schet = 0
 story_cost = []
--- HT_package/test_filemanager.py
+++ HT_package/test_filemanager.py
@@ -1,23 +1,12 @@
-'''
-3. В том же проекте создать модуль test_filemanager.py для тестирования функций консольного файлового менеджера
-4. В файле написать тесты для каждой ""чистой"" функции, чем больше тем лучше.
-Это могут быть функции консольного файлового менеджера, а так же программы мой счет и программы викторина
-'''
-
-
-
-# тестирование чистойфункции из файлового менеджера gjkefdnjvfnbxtcrjt
-'''
-from HT_10_def_schet import function_for_test
-
-list_in = [1, 2, 3, 'abc']
-assert function_for_test(list_in) == , ' error'
-'''
-
-from HT_10_def_schet import salary, hello_who
+from HT_package.def_for_test_class import salary, hello_who
 
 assert hello_who('Max') == 'Hello, Max', 'Hello who error'
 assert hello_who('Leo') == 'Hello, Leo', 'Hello who error'
 
 assert salary(2, 1) == 4, salary(2, 1)
 assert salary(2, 2) == 8
+
+from def_for_test_class import function_for_test
+
+list_in = [1, 2, 3, 'abc']
+assert function_for_test(list_in) == ' error'
