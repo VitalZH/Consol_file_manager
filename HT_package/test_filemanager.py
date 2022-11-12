1f804dcdc90ab5c213d94a0cb4405ff52c323a37
VitalZH <gmavik@gmail.com>
2022-11-12 13:11:30 +0300

    Добавлен файл est_python.py

--- HT_package/test_filemanager.py
+++ HT_package/test_filemanager.py
@@ -1,12 +1,24 @@
 from HT_package.def_for_test_class import salary, hello_who
 
-assert hello_who('Max') == 'Hello, Max', 'Hello who error'
-assert hello_who('Leo') == 'Hello, Leo', 'Hello who error'
+def test_hello_who():
+    assert hello_who('Maxx') == 'Hello, Max'
 
-assert salary(2, 1) == 4, salary(2, 1)
-assert salary(2, 2) == 8
+def test2_hello_who():
+    assert hello_who('Leo') == 'Hello, Leoxxx', 'Ошибка'
+
+def test_salary():
+    assert salary(2, 1) == 3, salary(2, 1)
+
+def test2_salary():
+    assert salary(2, 2) == 8
 
 from HT_package.def_for_test_class import function_for_test
 
 list_in = [1, 2, 3, 'abc']
-assert function_for_test(list_in) == ' error'
+def test_def_for_test_class():
+    assert function_for_test(list_in) == [1, 2, 3, 's']
+
+from HT_package.HT_9_victory import get_random_person
+
+def test_get_random_person():
+    assert get_random_person() == ('Александр Сергеевич Пушнин', '26.06.1799')
--- HT_package/test_python_second.py
+++
@@ -1,4 +0,0 @@
-from HT_package.HT_9_victory import get_random_person
-
-def test_get_random_person():
-    assert get_random_person() == ('Александр Сергеевич Пушнин', '26.06.1799')
