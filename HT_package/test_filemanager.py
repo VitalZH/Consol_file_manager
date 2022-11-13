8e3e5a86596e3d922854b0de99081580c860e23d
VitalZH <gmavik@gmail.com>
2022-11-13 12:08:43 +0300

    Добавлен файл est_python.py

--- HT_package/test_filemanager.py
+++ HT_package/test_filemanager.py
@@ -2,13 +2,13 @@ from HT_package.HT_9_victory import salary, hello_who, get_Pushkin
 
 
 def test_hello_who():
-    assert hello_who('Maxx') == 'Hello, Max'
+    assert hello_who('Max') == 'Hello, Max'
 
 def test2_hello_who():
-    assert hello_who('Leo') == 'Hello, Leoxxx', 'Ошибка'
+    assert hello_who('Leo') == 'Hello, Leo', 'Ошибка'
 
 def test_salary():
-    assert salary(2, 1) == 3, salary(2, 1)
+    assert salary(2, 1) == 4, salary(2, 1)
 
 def test2_salary():
     assert salary(2, 2) == 8
