66070bbcefe2a5be77bc335045d42a67dfb7489a
VitalZH <gmavik@gmail.com>
2023-02-22 07:37:59 +0300

    Добавлен файл est_python.py

--- HT_package/proverka.py
+++ HT_package/proverka.py
@@ -19,29 +19,26 @@ import random
 from random import randint
 
 # 1.блок выбора 15 случайных чисел для карточки
-list_15 = [randint(0, 90) for i in range(15)] # генератор списка из 15 случайных чисел от 0 до 90
+list_15 = [randint(0, 90) for i in range(15)]  # генератор списка из 15 случайных чисел от 0 до 90
 print(list_15)
+#from_list_15 = list_15.slice(2, 4)
+#print('from_list', from_list_15)
+my_list = [1, 2, 3, 4, 5, 6]
+first_part, middle_part, last_part = my_list.split(2, 4)
 
 # 2.блок реализации пустой карточки для игры Лото
 blank_karta = [['1', '-', '-', '-', '-', '-', '-', '-', '-'],
                ['-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-'],
                ['-', '-', '-', '-', '-', '-', '-', '-', '9']]
 for row in blank_karta:
-    poz_in_lin = sorted(random.sample(range(1, 10), 5)) # генератор позиц (неповтор) в линии карты (range +1 не включая последний элемент)
+    poz_in_lin = sorted(random.sample(range(1, 10),
+                                      5))  # генератор позиц (неповтор) в линии карты (range +1 не включая последний элемент)
     print('poz_in_lin;', poz_in_lin)
     for i in range(len(list_15[0:5])):
         print(f' инд в 5: {i}, знач из 15:{list_15[i]}, ном в лин:{poz_in_lin[i]}')
-        #if list_15[i] in poz_in_lin:
-            #blank_karta.remove(list_15[i])
-    print(' '.join(str(elem) for elem in row)) #Вывод на экран карточки, используя генератора строки из списка через Join
+        # if list_15[i] in poz_in_lin:
+        # blank_karta.remove(list_15[i])
+    print(' '.join(
+        str(elem) for elem in row))  # Вывод на экран карточки, используя генератора строки из списка через Join
 
 print(poz_in_lin)
-
-def get_list_index(blank_karta):
-    for i, line in enumerate(blank_karta):
-        print(i, line)
-        if line[0] == '1' and line[-1] == '9':
-            return i
-
-list_index = get_list_index(blank_karta)
-print('def', i)
