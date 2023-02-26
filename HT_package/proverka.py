930c0c40e09b34609021054b89c1f5246ce37913
gmavi <gmavik@gmail.com>
2023-02-26 11:34:46 +0300

    два варианта заполнениия blank_karta

--- HT_package/proverka.py
+++ HT_package/proverka.py
@@ -18,27 +18,52 @@ print(f'num#13 {s[1]}')
 import random
 from random import randint
 
-# 1.блок выбора 15 случайных чисел для карточки
+# 1.блок выбора 15 случайных чисел для карточки и разбиваю на три вложенных списка
 list_15 = [randint(0, 90) for i in range(15)]  # генератор списка из 15 случайных чисел от 0 до 90
 print(list_15)
-#from_list_15 = list_15.slice(2, 4)
-#print('from_list', from_list_15)
-my_list = [1, 2, 3, 4, 5, 6]
-first_part, middle_part, last_part = my_list.split(2, 4)
+split_list_15 = [list_15[:5], list_15[5:10], list_15[10:]]
+print(split_list_15)
 
 # 2.блок реализации пустой карточки для игры Лото
 blank_karta = [['1', '-', '-', '-', '-', '-', '-', '-', '-'],
                ['-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-'],
                ['-', '-', '-', '-', '-', '-', '-', '-', '9']]
 for row in blank_karta:
-    poz_in_lin = sorted(random.sample(range(1, 10),
-                                      5))  # генератор позиц (неповтор) в линии карты (range +1 не включая последний элемент)
-    print('poz_in_lin;', poz_in_lin)
-    for i in range(len(list_15[0:5])):
-        print(f' инд в 5: {i}, знач из 15:{list_15[i]}, ном в лин:{poz_in_lin[i]}')
-        # if list_15[i] in poz_in_lin:
-        # blank_karta.remove(list_15[i])
-    print(' '.join(
-        str(elem) for elem in row))  # Вывод на экран карточки, используя генератора строки из списка через Join
+    print('print row', row)
+    # генератор позиц (неповтор) в линии карты (range +1 не включая последний элемент)
+    ind_in_lin = sorted(random.sample(range(0, 10), 5))
+    print('ind_in_lin;', ind_in_lin)
+    # заполнение позиц в линии карты значениями из split_list_15
+    for ind in ind_in_lin:
+        row[ind] = split_list_15[row[] ][ind]
+    print('print row', row)
+    # Вывод на экран карточки, используя генератора строки из списка через Join
+    #print(' '.join(str(elem) for elem in row))
+#print(blank_karta)
 
-print(poz_in_lin)
+'''
+blank_karta = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+               [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+               [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
+split_list_15 = [[67, 87, 37, 32, 53],
+                 [33, 29, 3, 48, 28],
+                 [1, 51, 14, 32, 24]]
+ind_in_lin = [0, 2, 4, 6, 8]
+
+
+print('--Вар 1- Мой -')
+for i in range(3):
+    for j, val in enumerate(ind_in_lin):
+        blank_karta[i][val] = split_list_15[i][j]
+    print(f'линия{i}: {blank_karta[i]}')
+print('-----')
+
+print('--вар 2 - Ира --')
+for i in range(len(blank_karta)):
+    num_in_split_15 = 0
+    for j in range(len(blank_karta[i])):
+        if j in ind_in_lin:
+            blank_karta[i][j] = split_list_15[i][num_in_split_15]
+            num_in_split_15 += 1
+    print(blank_karta[i])
+'''
